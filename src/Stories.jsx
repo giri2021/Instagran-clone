c57810d1f29f89a5161becae6a6fe8d9185c2760
Vignesh kumar <vk@Vigneshs-MacBook-Air.local>
2026-09-07 16:52:43 +0530

    initial commit

---
+++ src/Stories.jsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react"
+import{ useNavigate } from "react-router-dom"
+
+function Stories() {
+
+  const navigate = useNavigate();
+
+  const [Stories, setStories] = useState([]);
+
+  let tot = 0;
+
+  useEffect(() => {
+    fetch('http://localhost:3000/story')
+    .then((data) => data.json())
+    .then((data) => setStories(data))
+    .catch((err) => console.log(err))
+  },[]);
+  
+  return (
+    <div className="story d-flex" >
+      <div className="d-none">
+        {tot = Stories.length}
+      </div>
+      
+      {Stories.length > 0 ? (
+        Stories.map((story)=>(
+          <div key={story.id} className="mx-1" onClick={() => {navigate(`/story/${story.id}/${tot}`)}}>
+            <div className="gradient-border">
+              <img src={story.profileImage} alt='dp' className="story-dp rounded-circle"/>
+            </div>
+            <p className="text-truncate" style={{width:"50px"}}>{story.username}</p>
+          </div>
+        ))
+      ):(
+        <p>Loading</p>
+      )}
+    </div>
+  )
+}
+
+export default Stories
